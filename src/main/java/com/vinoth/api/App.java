79f02294ebf9503bf7cbd2e7cf4cb5c4ba5d8d9f
Vinoth Kumar Manickavasagam <vinoth.manicks@gmail.com>
2020-06-17 10:55:11 -0700

    Cleaned up code slightly

--- src/main/java/com/vinoth/api/App.java
+++ src/main/java/com/vinoth/api/App.java
@@ -21,27 +21,6 @@ public class App {
         HandlebarsTemplateEngine templateEngine = new HandlebarsTemplateEngine();
         ChatService history = new FileHistoryManagement.FileMemoryChatService("global");
 
-        get("/", (request, response) -> {
-            Float userInput1 = request.queryMap("input1").floatValue();
-            Float userInput2 = request.queryMap("input2").floatValue();
-
-            Map<String, Object> map = new HashMap<>();
-            map.put("input1", userInput1);
-            map.put("input2", userInput2);
-            map.put("operations", Operation.values());
-
-            if (userInput1 != null && userInput2 != null) {
-                String op = request.queryMap("op").value();
-                Operation operation = Operation.valueOf(op);
-                Calculation calculation = new Calculation(userInput1, userInput2);
-                float result = calculation.performOperation(operation);
-                map.put("operationType", op);
-                map.put("operationResult", result);
-            }
-
-            return templateEngine.render(new ModelAndView(map, "index.hbs"));
-        });
-
         get("/chat", (request, response) -> {
 
             Map<String, Object> map = new HashMap<>();
